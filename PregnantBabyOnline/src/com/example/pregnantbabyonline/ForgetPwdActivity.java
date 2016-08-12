498c0ee5694423502e0752279264dcb6e4c0d6a2
Xu2eshuang <1593060405@qq.com>
2016-08-12 17:32:32 +0800

    忘记密码

--- PregnantBabyOnline/src/com/example/pregnantbabyonline/ForgetPwdActivity.java
+++ PregnantBabyOnline/src/com/example/pregnantbabyonline/ForgetPwdActivity.java
@@ -1,8 +1,12 @@
 package com.example.pregnantbabyonline;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,13 +18,28 @@ public class ForgetPwdActivity extends Activity {
 	ImageButton returnLogin;
 	EditText phoneNum,code;
 	Button getCode,next;
-	@Override
+	private int countSeconds = 60;//倒计时秒数
+	private String usersuccess;
+	private Context context;
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_forget_password);
 		initView();
 	}
+	
+	private Handler countHandler = new Handler(){
+		public void handleMessage(Message msg){
+			super.handleMessage(msg);
+			if(countSeconds > 0){
+				--countSeconds;
+				getCode.setText("("+ countSeconds +")后获取验证码");
+				countHandler.sendEmptyMessageDelayed(0, 1000);
+			}else{
+				countSeconds = 60;
+				getCode.setText("请重新获取验证码");
+			}
+		}
+	};
 	
 	public void initView(){
 		returnLogin = (ImageButton) findViewById(R.id.back_login);
@@ -56,5 +75,18 @@ public class ForgetPwdActivity extends Activity {
 	public void nextToReset(){//跳转到重新设置密码界面
 		Intent intent = new Intent(ForgetPwdActivity.this, ResetPwdActivity.class);
 		startActivity(intent);
+	}
+	
+	public void getCode(){
+		if(countSeconds == 60){
+			String mobile = phoneNum.getText().toString();
+			getMobile(mobile);
+		}
+	}
+	
+	private void getMobile(String mobile){
+		if("".equals(mobile)){
+			new AlertDialog.Builder(context).setTitle("提示").setMessage("手机号码不正确");
+		}
 	}
 }
