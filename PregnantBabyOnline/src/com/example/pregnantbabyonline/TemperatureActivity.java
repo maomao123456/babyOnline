42fb11ed852e2186479439d920f93df09f228341
zhangjian3364 <1549172463@qq.com>
2016-08-15 08:52:35 +0800

    温度的Activity页面的增加

---
+++ PregnantBabyOnline/src/com/example/pregnantbabyonline/TemperatureActivity.java
@@ -0,0 +1,15 @@
+package com.example.pregnantbabyonline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemperatureActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_emperature);
+	}
+
+}
