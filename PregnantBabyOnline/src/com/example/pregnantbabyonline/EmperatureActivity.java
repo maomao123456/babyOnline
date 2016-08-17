ac3eba781edbd3e063a932413963037dc09fcda3
zhangjian3364 <1549172463@qq.com>
2016-08-17 10:35:22 +0800

    增加温度页面哈反馈页面的activity

---
+++ PregnantBabyOnline/src/com/example/pregnantbabyonline/EmperatureActivity.java
@@ -0,0 +1,15 @@
+package com.example.pregnantbabyonline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmperatureActivity  extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_emperature);
+	}
+
+}
